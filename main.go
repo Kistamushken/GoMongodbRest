40b2bc0e8e7a1129a7aecbecb26e8821862221d0
Phil <kistamushken@gmail.com>
2016-02-29 00:15:43 +0300

    added git ignore + some work on packages

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
-import ("users")
+import "users"
 /*
 Create a new MongoDB session, using a database
-named "signatures". Create a new server using
+named "users". Create a new server using
 that session, then begin listening for HTTP requests.
 */
 func main() {
